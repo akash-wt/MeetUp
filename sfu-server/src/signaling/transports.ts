bffc221fab2c633f8c0295a1d3fb6f34894a67e1
akash patel <akash906129@gmail.com>
2025-05-16 05:16:38 +0530

    vedio renerding fixed

--- sfu-server/src/signaling/transports.ts
+++ sfu-server/src/signaling/transports.ts
@@ -3,12 +3,22 @@ import { webRtcTransport_options } from "../config";
 
 export const createWebRtcTransport = async (router: mediasoupTypes.Router) => {
 
+    // const transport = await router.createWebRtcTransport({
+    //     ...webRtcTransport_options
+    // });
     const transport = await router.createWebRtcTransport({
-        ...webRtcTransport_options
+        listenIps: [{ ip: "192.168.240.135" }],
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
     });
 
-    console.log(`Created WebRTC Transport: ${transport.id}`);
+    transport.on("icestatechange", (state) =>
+        console.log(`Transport ${transport.id} ICE state: ${state}`)
+    );
     
+    console.log(`Created WebRTC Transport: ${transport.id}`);
+
     transport.on('dtlsstatechange', (state: mediasoupTypes.DtlsState) => {
         if (state === 'closed') {
             console.log(`Transport ${transport.id} closed`);
